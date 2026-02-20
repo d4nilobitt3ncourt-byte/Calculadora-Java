8b0ccf3dace560a47d495941b2057e88f35af2a4
d4nilobitt3ncourt-byte <d4nilo.bitt3ncourt@gmail.com>
2026-02-20 13:50:48 -0300

    Sistema de soma básico adicionado

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    public static double somar(double a, double b){
+            return a + b;
+        }
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Número 1: ");
+        double n1 = scan.nextDouble();
+
+        System.out.println("Número 2: ");
+        double n2 = scan.nextDouble();
+
+        double result = somar(n1, n2);
+        System.out.println("O resultado dessa soma é: " + result);
+        scan.close();
     }
 }
