b1717c0057262cb49c1e1e5e603bb48a87bb0b07
d4nilobitt3ncourt-byte <d4nilo.bitt3ncourt@gmail.com>
2026-02-20 14:27:09 -0300

    Sistema de soma e diferença implementados de forma dinâmica

--- Main.java
+++ Main.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    public static double somar(double a, double b){
-            return a + b;
+    public static double CalcularExpressao(String exp){
+            if (exp.contains("+")) {
+                String[] partes = exp.split("\\+");
+                double a = Double.parseDouble(partes[0]);
+                double b = Double.parseDouble(partes[1]);
+                return a+b;
+            }
+            else if (exp.contains("-")) {
+                String[] partes = exp.split("\\-");
+                double a = Double.parseDouble(partes[0]);
+                double b = Double.parseDouble(partes[1]);
+                return a-b;
+            }
+            else {
+                throw new IllegalArgumentException("Operação inválida, por favor escrever expressão corretamente");
+            }
         }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Número 1: ");
-        double n1 = scan.nextDouble();
-
-        System.out.println("Número 2: ");
-        double n2 = scan.nextDouble();
-
-        double result = somar(n1, n2);
-        System.out.println("O resultado dessa soma é: " + result);
+        System.out.println("Digite expressão de soma ou diferença (ex: 5+2, 4-3): ");
+        String entrada = scan.nextLine();
+        double result = CalcularExpressao(entrada);
+        System.out.println("Resultado: " + result);
         scan.close();
     }
 }
