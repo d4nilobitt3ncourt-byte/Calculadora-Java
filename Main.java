ee8b779df1a88e2641070dcd22630ec05a83ebdc
d4nilobitt3ncourt-byte <d4nilo.bitt3ncourt@gmail.com>
2026-02-23 16:16:02 -0300

    Sistema de expressões avançado implementado

--- Main.java
+++ Main.java
@@ -1,43 +1,53 @@
 import java.util.Scanner;
 
 public class Main {
-    public static double CalcularExpressao(String exp){
-            if (exp.contains("+")) {
-                String[] partes = exp.split("\\+");
-                double a = Double.parseDouble(partes[0]);
-                double b = Double.parseDouble(partes[1]);
-                return a+b;
-            }
-            else if (exp.contains("-")) {
-                String[] partes = exp.split("\\-");
-                double a = Double.parseDouble(partes[0]);
-                double b = Double.parseDouble(partes[1]);
-                return a-b;
-            }
-            else if (exp.contains("*")) {
-                String[] partes = exp.split("\\*");
-                double a = Double.parseDouble(partes[0]);
-                double b = Double.parseDouble(partes[1]);
-                return a*b;
-            }
-            else if (exp.contains("/")) {
-                String[] partes = exp.split("\\/");
-                double a = Double.parseDouble(partes[0]);
-                double b = Double.parseDouble(partes[1]);
-                if (a == 0 || b == 0) {
-                    throw new ArithmeticException("Divisão por zero não é permitida!");
-                }
-                return a/b;
-            }
-            else {
-                throw new IllegalArgumentException("Operação inválida! por favor escrever expressão corretamente");
-            }
+    public static double calcularExpressao(String exp) {
+
+    exp = exp.trim().replaceAll("\\s+", "");
+
+    double resultado = 0;
+    double numeroAtual = 0;
+    char operador = '+';
+
+    for (int i = 0; i < exp.length(); i++) {
+
+        char c = exp.charAt(i);
+
+        if (Character.isDigit(c) || c == '.') {
+            numeroAtual = numeroAtual * 10 + (c - '0');
         }
+
+        if (!Character.isDigit(c) || i == exp.length() - 1) {
+
+            switch (operador) {
+                case '+':
+                    resultado += numeroAtual;
+                    break;
+                case '-':
+                    resultado -= numeroAtual;
+                    break;
+                case '*':
+                    resultado *= numeroAtual;
+                    break;
+                case '/':
+                    if (numeroAtual == 0)
+                        throw new ArithmeticException("Divisão por zero");
+                    resultado /= numeroAtual;
+                    break;
+            }
+
+            operador = c;
+            numeroAtual = 0;
+        }
+    }
+
+    return resultado;
+}
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Digite expressão de soma ou diferença (ex: 5+2, 4-3, 5*2,6/2): ");
         String entrada = scan.nextLine();
-        double result = CalcularExpressao(entrada);
+        double result = calcularExpressao(entrada);
         System.out.println("Resultado: " + result);
         scan.close();
     }
