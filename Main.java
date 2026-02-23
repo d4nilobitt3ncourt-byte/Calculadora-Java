61df0b2c0cfb88f0cf414c194375dafe85b4a9e5
d4nilobitt3ncourt-byte <d4nilo.bitt3ncourt@gmail.com>
2026-02-23 15:06:33 -0300

    Sistema de divisão e multiplicação implementados!

--- Main.java
+++ Main.java
@@ -14,13 +14,28 @@ public class Main {
                 double b = Double.parseDouble(partes[1]);
                 return a-b;
             }
+            else if (exp.contains("*")) {
+                String[] partes = exp.split("\\*");
+                double a = Double.parseDouble(partes[0]);
+                double b = Double.parseDouble(partes[1]);
+                return a*b;
+            }
+            else if (exp.contains("/")) {
+                String[] partes = exp.split("\\/");
+                double a = Double.parseDouble(partes[0]);
+                double b = Double.parseDouble(partes[1]);
+                if (a == 0 || b == 0) {
+                    throw new ArithmeticException("Divisão por zero não é permitida!");
+                }
+                return a/b;
+            }
             else {
-                throw new IllegalArgumentException("Operação inválida, por favor escrever expressão corretamente");
+                throw new IllegalArgumentException("Operação inválida! por favor escrever expressão corretamente");
             }
         }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite expressão de soma ou diferença (ex: 5+2, 4-3): ");
+        System.out.println("Digite expressão de soma ou diferença (ex: 5+2, 4-3, 5*2,6/2): ");
         String entrada = scan.nextLine();
         double result = CalcularExpressao(entrada);
         System.out.println("Resultado: " + result);
