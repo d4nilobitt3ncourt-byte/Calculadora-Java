6b807c83821bd8d6d6dac3f85d8c2ea8939b006c
d4nilobitt3ncourt-byte <d4nilo.bitt3ncourt@gmail.com>
2026-03-05 16:29:36 -0300

    Calculadora agora respeitando ordem de sinais e decimais

--- Main.java
+++ Main.java
@@ -1,44 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static double calcularExpressao(String exp) {
-    exp = exp.trim().replaceAll("\\s+", "");
-    double resultado = 0;
-    String numeroAtual = "";
-    char operador = '+';
-    for (int i = 0; i < exp.length(); i++) {
-        char c = exp.charAt(i);
-        if (Character.isDigit(c) || c == '.') {
-            numeroAtual += c;
-        }
-        if ((!Character.isDigit(c) && c != '.') || i == exp.length() - 1) {
-            double num = Double.parseDouble(numeroAtual);
-            switch (operador) {
-                case '+':
-                    resultado += num;
-                    break;
-                case '-':
-                    resultado -= num;   
-                    break;
-                case '*':
-                    resultado *= num;
-                    break;
-                case '/':
-                    if (num == 0)
-                        throw new ArithmeticException("Divisão por zero");
-                    resultado /= num;
-                    break;
-            }
-            operador = c;
-            numeroAtual = "";
-        }
-    }
+        exp = exp.trim().replaceAll("\\s+", "");
 
-    return resultado;
-}
+        List<Double> numeros = new ArrayList<>();
+        List<Character> operadores = new ArrayList<>();
+
+        String numeroAtual = "";
+
+        for (int i = 0; i < exp.length(); i++){
+            char c = exp.charAt(i);
+            if (Character.isDigit(c) || c == '.'){  
+                numeroAtual += c;
+            }
+            else{
+                numeros.add(Double.parseDouble(numeroAtual));
+                numeroAtual = "";
+
+                operadores.add(c);
+            }
+        }
+        numeros.add(Double.parseDouble(numeroAtual));
+        
+        for (int i = 0; i<operadores.size(); i++){
+            char op = operadores.get(i);
+            if (op == '*' || op == '/') {
+                double a = numeros.get(i);
+                double b = numeros.get(i+1);
+                double resultado;
+                if (op == '*'){
+                    resultado = a*b;
+                }
+                else {
+                    if (b == 0 || a == 0)
+                        throw new ArithmeticException("Divisão por zero não pode!");
+                    resultado = a/b;
+                }
+                numeros.set(i,resultado);
+                numeros.remove(i+1);
+                operadores.remove(i);
+
+                i--;
+            }
+        }
+        double resultado = numeros.get(0);
+        for (int i = 0; i < operadores.size(); i++){
+            char op = operadores.get(i);
+            double num = numeros.get(i+1);
+            if (op == '+'){
+                resultado += num;
+            }
+            else{
+                resultado -= num;
+            }
+        }
+        return resultado;
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite expressão (ex: 5+2, 4-3, 5*2,6/2): ");
+        System.out.println("Digite uma expressão(Ex: 1+2, 2-1, 4+2*3): ");
         String entrada = scan.nextLine();
         double result = calcularExpressao(entrada);
         System.out.println("Resultado: " + result);
