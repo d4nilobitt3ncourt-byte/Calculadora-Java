0ef04ba8d172587bb3a5f39e06743175c8d6f044
d4nilobitt3ncourt-byte <d4nilo.bitt3ncourt@gmail.com>
2026-03-18 15:17:05 -0300

    Remodulação do sistema

--- Main.java
+++ Main.java
@@ -3,14 +3,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main{
-    public static double calcularExpressao(String exp) {
+    // Parser para montar a expressão.
+    public static void parser(String exp, List<Double> numeros, List<Character> operadores){
         exp = exp.trim().replaceAll("\\s+", "");
-
-        List<Double> numeros = new ArrayList<>();
-        List<Character> operadores = new ArrayList<>();
-
         String numeroAtual = "";
-        
         for (int i = 0; i < exp.length(); i++){
             char c = exp.charAt(i);
             if (Character.isDigit(c) || c == '.'){  
@@ -24,69 +20,65 @@ public class Main{
             }
         }
         numeros.add(Double.parseDouble(numeroAtual));
-        
-        for (int i = operadores.size()-1;i>=0; i--){
+    }
+    //Calcular portencia e raiz
+    public static void potenciaEraiz(List<Double>numeros, List<Character> operadores){
+        for (int i = operadores.size()-1; i>=0; i--){
             char op = operadores.get(i);
-            if (op == 'r') {
+            if (op == 'r'||op == '^') {
                 double a = numeros.get(i);
                 double b = numeros.get(i+1);
-                double resultado = Math.pow(b, 1.0/a);
-                numeros.set(i,resultado);
+                double resultado = (op == 'r') ? Math.pow(b, 1.0/a) : Math.pow(a,b);
+                numeros.set(i, resultado);
                 numeros.remove(i+1);
-                operadores.remove(i); 
+                operadores.remove(i);
             }
         }
-
-        for (int i = operadores.size()-1;i>=0; i--){
+    }
+    //calcular divisão, multiplicação e porcentagem
+    public static void divEmultiEporcent(List<Double>numeros, List<Character> operadores){
+        for (int i = 0; i<operadores.size();i++) {
             char op = operadores.get(i);
-            if (op == '^') {
-                double a = numeros.get(i);
-                double b = numeros.get(i+1);
-                double resultado = Math.pow(a,b);
-                numeros.set(i,resultado);
-                numeros.remove(i+1);
-                operadores.remove(i); 
-            }
-        }
-
-        for (int i = 0; i<operadores.size(); i++){
-            char op = operadores.get(i);
-            if (op == '*' || op == '/') {
+            if (op == '*' || op == '/' || op == '%') {
                 double a = numeros.get(i);
                 double b = numeros.get(i+1);
                 double resultado;
-                if (op == '*'){
-                    resultado = a*b;
-                }
-                else {
-                    if (b == 0 || a == 0)
-                        throw new ArithmeticException("Divisão por zero não pode!");
-                    resultado = a/b;
-                }
-                numeros.set(i,resultado);
+                if (op == '*') resultado = a*b;
+                else if (op == '/') resultado = a/b;
+                else resultado = a * (b/100);
+                numeros.set(i, resultado);
                 numeros.remove(i+1);
                 operadores.remove(i);
-
                 i--;
             }
         }
+    }
+    //calcular soma e diferença
+    public static Double somaEdiferenca(List<Double>numeros, List<Character> operadores){
         double resultado = numeros.get(0);
         for (int i = 0; i < operadores.size(); i++){
             char op = operadores.get(i);
             double num = numeros.get(i+1);
-            if (op == '+'){
-                resultado += num;
-            }
-            else{
-                resultado -= num;
-            }
+            if (op == '+') resultado += num;
+            else resultado -= num;
         }
         return resultado;
     }
+    //Executar toda a lógica e a prioridade matemática de uma só vez
+    public static double calcularExpressao(String exp){
+        List<Double> numeros = new ArrayList<>();
+        List<Character> operadores = new ArrayList<>();
+
+        parser(exp, numeros, operadores);
+        potenciaEraiz(numeros, operadores);
+        divEmultiEporcent(numeros, operadores);
+        return somaEdiferenca(numeros, operadores);
+    }
+    //entrada do usuário
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Digite uma expressão(Ex: 1+2, 1-2, 1*2, 1/2, 2^3, 2r4): ");
-        String entrada = scan.nextLine();
+        System.out.println("Digite uma expressão(Ex: 1+2, 1-2, 1*2, 1/2, 2^3, 2r4, 10%20): ");
+        String entrada = scan.nextLine();   
         double result = calcularExpressao(entrada);
         System.out.println("Resultado: " + result);
         scan.close();
